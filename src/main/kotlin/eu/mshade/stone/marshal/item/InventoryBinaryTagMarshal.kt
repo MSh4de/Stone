a3ef1fdb7c9bc697b53edbc4416e269d2234d8f6
RealAlphaUA <realalpha.ua@gmail.com>
2022-12-06 00:08:07 +0100

    add uniqueId into serializer/deserializer 🧶

--- src/main/kotlin/eu/mshade/stone/marshal/item/InventoryBinaryTagMarshal.kt
+++ src/main/kotlin/eu/mshade/stone/marshal/item/InventoryBinaryTagMarshal.kt
@@ -1,6 +1,5 @@
 package eu.mshade.stone.marshal.item
 
-import eu.mshade.enderchest.marshal.mojang.TextComponentBinaryTagMarshal
 import eu.mshade.enderframe.inventory.ChestInventory
 import eu.mshade.enderframe.inventory.Inventory
 import eu.mshade.enderframe.inventory.InventoryKey
@@ -12,21 +11,24 @@ import eu.mshade.enderframe.mojang.chat.TextComponent
 import eu.mshade.mwork.binarytag.BinaryTagType
 import eu.mshade.mwork.binarytag.entity.CompoundBinaryTag
 import eu.mshade.mwork.binarytag.entity.ListBinaryTag
+import eu.mshade.stone.marshal.mojang.TextComponentBinaryTagMarshal
+import java.util.*
 import java.util.function.Supplier
 
 object InventoryBinaryTagMarshal {
 
-    private val INVENTORY_BY_ID = mutableMapOf<InventoryKey, (txt: TextComponent, size: Int) -> Inventory>()
+    private val INVENTORY_BY_ID = mutableMapOf<InventoryKey, (txt: TextComponent, size: Int, uniqueId: UUID) -> Inventory>()
 
     init {
-        INVENTORY_BY_ID[InventoryType.CHEST] = {txt, size -> ChestInventory(txt, size)}
-        INVENTORY_BY_ID[InventoryType.PLAYER] = { _, _ -> PlayerInventory() }
+        INVENTORY_BY_ID[InventoryType.CHEST] = {txt, size, uniqueId -> ChestInventory(txt, size, uniqueId)}
+        INVENTORY_BY_ID[InventoryType.PLAYER] = { _, _, uniqueId -> PlayerInventory(uniqueId) }
     }
 
     fun serialize(inventory: Inventory, metadataKeyValueBufferRegistry: MetadataKeyValueBufferRegistry): CompoundBinaryTag{
         val compoundBinaryTag = CompoundBinaryTag()
         compoundBinaryTag.putInt("type", inventory.inventoryKey.id)
         compoundBinaryTag.putInt("size", inventory.size)
+        compoundBinaryTag.putString("uniqueId", inventory.uniqueId.toString())
 
         if (inventory is NamedInventory){
             compoundBinaryTag.putBinaryTag("name", TextComponentBinaryTagMarshal.serialize(inventory.name))
@@ -44,9 +46,10 @@ object InventoryBinaryTagMarshal {
     fun deserialize(compoundBinaryTag: CompoundBinaryTag, metadataKeyValueBufferRegistry: MetadataKeyValueBufferRegistry): Inventory{
         val type = InventoryType.fromId(compoundBinaryTag.getInt("type"))!!
         val size = compoundBinaryTag.getInt("size")
+        val uniqueId = UUID.fromString(compoundBinaryTag.getString("uniqueId"))
         val nameBinaryTag = compoundBinaryTag.getBinaryTag("name")
         val name = if (nameBinaryTag != null) TextComponentBinaryTagMarshal.deserialize(nameBinaryTag as CompoundBinaryTag) else TextComponent.empty()
-        val inventory = INVENTORY_BY_ID[type]?.invoke(name, size) ?: NamedInventory(name, type)
+        val inventory = INVENTORY_BY_ID[type]?.invoke(name, size, uniqueId) ?: NamedInventory(name, type, uniqueId)
         val itemListBinaryTag = compoundBinaryTag.getBinaryTag("items") as ListBinaryTag
         itemListBinaryTag.value.forEachIndexed{index, binaryTag ->
             inventory.setItemStack(index, ItemStackBinaryTagMarshal.deserialize(binaryTag as CompoundBinaryTag, metadataKeyValueBufferRegistry))
