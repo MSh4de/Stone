375440d8c75d16e1f60abf4d6cb0b09436c64789
RealAlphaUA <realalpha.ua@gmail.com>
2022-12-18 23:56:48 +0100

    clean code 🛒

--- src/main/kotlin/eu/mshade/stone/packet/world/AxolotlPacketOutInitializeWorld.kt
+++ src/main/kotlin/eu/mshade/stone/packet/world/AxolotlPacketOutInitializeWorld.kt
@@ -8,7 +8,7 @@ import eu.mshade.mwork.binarytag.entity.CompoundBinaryTag
 class AxolotlPacketOutInitializeWorld(val world: World, private val metadataKeyValueBufferRegistry: MetadataKeyValueBufferRegistry): AxolotlPacketOut {
 
     override fun write(compoundBinaryTag: CompoundBinaryTag) {
-        compoundBinaryTag.putBinaryTag("world",metadataKeyValueBufferRegistry.serialize(world.metadataKeyValueBucket))
+        compoundBinaryTag.putBinaryTag("world", metadataKeyValueBufferRegistry.serialize(world.metadataKeyValueBucket))
     }
 
 }
